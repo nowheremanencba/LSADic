faa4e36fc141660ed9bf1abacb9a4fec94f4a5f1
Cristian Ortega <cristian.ortega@tipsalud.local>
2019-11-05 16:52:37 -0300

    adminVideos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+// Firebase
 import { AuthenticationService } from './services/authentication.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
- 
 import * as firebase from 'firebase';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+// Angular Material Module 
 import { AppMaterialModule } from './app-material.module';
-
 firebase.initializeApp(environment.firebase);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,BrowserAnimationsModule, AppMaterialModule, 
-    AngularFireAuthModule, ReactiveFormsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+    AppRoutingModule,BrowserAnimationsModule, 
+    AppMaterialModule, 
+    AngularFireAuthModule, 
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/pages/videos-admin/videos-admin.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { VideosAdminPage } from './videos-admin.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: VideosAdminPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [VideosAdminPage]
+})
+export class VideosAdminPageModule {}
